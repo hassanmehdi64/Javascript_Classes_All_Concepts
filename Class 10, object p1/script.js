fc228da5565a656c72a4e627c161070da32e88a2
hassanmehdi64 <hassanshafa64@gmail.com>
2023-12-12 22:55:20 -0800

    class 10 part 1 object

--- Class 10, object p1/script.js
+++ Class 10, object p1/script.js
@@ -1,12 +1,12 @@
 // object literals
 
-// const mySym = Symbol("key1")
 
+const mySym = Symbol("key1")
 
 const JsUser = {
-    name: "aryan",
-    // "full name": "aryan ali",
-    // [mySym]: "mykey1",
+    name: "waqar",
+    ['full name'] : 'waqar ali',
+   [ mySym]: "mykey1",
     age: 18,
     location: "danyor",
     email: "abc@gmail.com",
@@ -14,23 +14,30 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
 
-// JsUser.email = "abcd@gmail.com"
+// console.log(JsUser.name)
+
+// console.log(JsUser['full name'])
+// console.log(typeof JsUser.mySym)
+
+JsUser.email = "abcd@gmail.com"
+console.log(JsUser)
+
 // Object.freeze(JsUser)
+
 // JsUser.email = "abcd@gmail123.com"
+
+
+JsUser.greeting = function(){
+    console.log("Hello JS user ");
+}
 // console.log(JsUser);
 
-// JsUser.greeting = function(){
-//     console.log("Hello JS user");
-// }
 
-// JsUser.greetingTwo = function(){
-//     console.log(`Hello JS user, ${this.name}`);
-// }
 
-// console.log(JsUser.greeting());
-// console.log(JsUser.greetingTwo());
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
