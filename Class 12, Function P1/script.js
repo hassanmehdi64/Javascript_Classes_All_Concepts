19d57a2270b7ac689d162322df6e57d6c651e7c3
hassanmehdi64 <hassanshafa64@gmail.com>
2023-12-14 22:32:31 -0800

    class 12 function p1

---
+++ Class 12, Function P1/script.js
@@ -0,0 +1,56 @@
+// function sayMyName(){
+//     console.log("S");
+//         console.log("A");
+//         console.log("k");
+//         console.log("H");
+//         console.log("A");
+//         console.log("W");
+//         console.log("A");
+//         console.log("T");
+// }
+
+// sayMyName()
+
+
+
+
+// function addTwoNumbers(number1, number2){
+
+//         number1  + number2
+
+// }
+
+// console.log(addTwoNumbers(1, 4));;
+
+
+
+
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+
+    // return  number1 + number2
+}
+
+
+// const resul = addTwoNumbers(3, 5)
+
+// console.log("Result: ", resul);
+
+
+
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("PLease enter a username");
+      return
+    }
+    return `${username} just logged in`
+
+}
+
+console.log(loginUserMessage('sakhawat'))
+console.log(loginUserMessage('hello'))
+
