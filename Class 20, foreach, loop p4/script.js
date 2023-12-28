0e29cdf41111d89d7dba6137ba535a9e587d9b70
hassanmehdi64 <hassanshafa64@gmail.com>
2023-12-27 22:22:13 -0800

    class20 foreach p4

---
+++ Class 20, foreach, loop p4/script.js
@@ -0,0 +1,50 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach(function (elements) {
+//     console.log(elements);
+// })
+
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+
+// -----------------------------------------
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// ---------------------------------------------------------
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+
+
+// ----------------------------------------------------------
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+// console.log(myCoding[0].languageFileName);
+
+// myCoding.forEach( (item) => {
+    
+//     console.log(item.languageName);
+// } )
