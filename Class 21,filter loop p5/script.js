2c463f361b1fc7c83bdd2d739150e39278ddddfd
hassanmehdi64 <hassanshafa64@gmail.com>
2024-01-01 07:32:38 -0800

    class 21

---
+++ Class 21,filter loop p5/script.js
@@ -0,0 +1,64 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+//     // return item
+// } )
+
+// console.log(values);
+
+
+// ---------------------- Filter -----------------------
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter((value) => (value <= 6) )
+
+// console.log(newNums);
+
+
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
+
+
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+//   ];
+
+// //   let userBooks = books.filter( (boks) => boks.genre === 'History')
+
+//   userBooks = books.filter( (books) => { 
+//     return books.publish >= 1995 && books.genre === "History"
+// })
+
+//   console.log(userBooks);
+
+
+
+//   ----------------------------------------------------
+
+function isBigEnough(value) {
+    return value >= 10;
+  }
+  const arr =  [12, 5, 8, 130, 44];
+  const filtered = arr.filter(isBigEnough);
+
+console.log(filtered);
+  
