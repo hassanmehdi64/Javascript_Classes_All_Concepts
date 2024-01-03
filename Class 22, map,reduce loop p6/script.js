2b3ba675de2846b16c0203d22acde21f82d6e4fe
hassanmehdi64 <hassanshafa64@gmail.com>
2024-01-02 22:59:14 -0800

    class 22 map, reduce

---
+++ Class 22, map,reduce loop p6/script.js
@@ -0,0 +1,22 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const mynum = myNumers.map((value, index, array) => {
+//     // return value + 10
+//     console.log(`value is ${value}, index ${index}, arr, ${array}`);
+// })
+
+// console.log(mynum);
+
+// const newNums = myNumers.map( (num) =>  num + 10)
+
+
+
+// ---------Chaining----------------
+
+
+// const newNums = myNumers
+//                 .map((num) => num * 10 )
+//                 .map( (num) => num + 1)
+//                 .filter( (num) => num >= 40)
+
+// console.log(newNums);
